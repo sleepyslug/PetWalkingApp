9de4f0cebb922b4d61ba6d9b7f10f903741df765
Salvador Garcia <sal.garcia.three@gmail.com>
2019-09-03 11:16:36 -0700

    Initial Commit

---
+++ PetWalkingApp.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PetWalkingApp.API.Models;
+
+namespace PetWalkingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
---
+++ PetWalkingApp.API/Models/Value.cs
@@ -0,0 +1,8 @@
+namespace PetWalkingApp.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
