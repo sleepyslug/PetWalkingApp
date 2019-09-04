5c18ef4b67f430be34c7a0191dd75fef2346ef81
Salvador Garcia <sal.garcia.three@gmail.com>
2019-09-03 19:04:16 -0700

    End of Section 3 - Add Security/User Reg

--- PetWalkingApp.API/Controllers/ValuesController.cs
+++ PetWalkingApp.API/Controllers/ValuesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PetWalkingApp.API.Data;
@@ -9,6 +10,7 @@ using PetWalkingApp.API.Data;
 namespace PetWalkingApp.API.Controllers
 {
     // http://localhost:5000/api/values
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -29,6 +31,7 @@ namespace PetWalkingApp.API.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
--- PetWalkingApp.API/Data/DataContext.cs
+++ PetWalkingApp.API/Data/DataContext.cs
@@ -8,5 +8,7 @@ namespace PetWalkingApp.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options) {}
 
         public DbSet<Value> Values { get; set; }
+
+        public DbSet<User> Users { get; set; }
     }
 }
